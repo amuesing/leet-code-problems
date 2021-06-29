6580a219b783cd52fe2b8867413b6a078f0a6459
Aaron Muesing <75646254+muesinga@users.noreply.github.com>
2021-06-28 22:53:22 -0400

    Time: 76 ms (72.45%), Space: 40 MB (12.24%) - LeetHub

---
+++ search-insert-position/search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let i = 0
+    let j = nums.length - 1
+    
+    while (i <= j) {
+        const mid = Math.floor((i + j) / 2)
+        
+        if (nums[mid] === target) {
+            return mid
+        } else if (nums[mid] < target) {
+            i = mid + 1
+        } else {
+            j = mid - 1
+        }
+    } 
+    return j + 1
+};
