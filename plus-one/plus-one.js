bdad396bda038e7dd5affe49a9009ab69ee820e9
Aaron Muesing <75646254+muesinga@users.noreply.github.com>
2021-07-12 20:25:28 -0400

    Time: 80 ms (44.55%), Space: 39.1 MB (9.70%) - LeetHub

---
+++ plus-one/plus-one.js
@@ -0,0 +1,8 @@
+var plusOne = function(digits) {
+    const fromArray = digits.join(''); // get Digits out of Array
+    const toInt = BigInt(fromArray) ; //Dont use parseInt, use BigInt instead :) 
+    const addOne = toInt + 1n; // add one! 
+    const result =  Array.from(String(addOne), Number); // return digits to array
+    return result
+    
+};
