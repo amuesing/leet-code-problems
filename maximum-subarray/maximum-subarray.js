b1d16621798b7ff5d647e44805be7dc702e441ce
Aaron Muesing <75646254+muesinga@users.noreply.github.com>
2021-07-26 13:16:06 -0400

    Time: 80 ms (69.63%), Space: 40.4 MB (24.02%) - LeetHub

---
+++ maximum-subarray/maximum-subarray.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let maxSum = nums[0];
+    
+    for (let i = 1; i < nums.length; i++) { 
+        nums[i] = Math.max(nums[i], nums[i] + nums[i - 1]);
+        maxSum = Math.max(maxSum, nums[i]);
+    }
+    
+    return maxSum;
+};
